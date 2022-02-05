6f0a1f79641c41ba5a3b2240705114a1d864c7d9
jinwoo <jinu29118@gmail.com>
2022-02-05 18:21:58 +0900

    회원가입(신체정보입력) 액티비티 생성
    회원가입 두 번째 화면인 신체정보 입력 화면생성
    No speakable text present
    라는 오류는 추후 다시 확인 필요

---
+++ app/src/main/java/com/example/art_run_android/BodyInformEditActivity.kt
@@ -0,0 +1,11 @@
+package com.example.art_run_android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BodyInformEditActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_body_inform_edit)
+    }
+}
