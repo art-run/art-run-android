c1600417f2a6ba6a53ae3aa932c5ec4393421d19
jinwoo <jinu29118@gmail.com>
2022-02-16 12:06:19 +0900

    로그인 페이지, 정보수정 페이지 기본 업데이트
    0. 앞으로 제가 수정하는 것들은 뷰 바인딩을 사용할 것 같습니다..!
        => gradle은 수정함.
    
    1. 기존엔 러닝메인 페이지가 앱 초기화면이었으나, 로그인 페이지를 첫 화면으로 변경함
        => Manifest 수정함.
    
    2. 로그인 페이지에서는 사용자가 입력한 ID와 비밀번호 정보를 잠시 저장하여 추후 서버 api와 연동할 수 있게끔 함.
    (우선 현재는 사용자가 입력한 ID와 비밀번호를 그대로 토스트로 띄워주며 입력받은 내용이 어떤 것인지를 확인할 수 있음을 체크하는 용도로 메시지가 뜸.)
        2-1. 로그인 페이지에서 회원가입 버튼을 클릭 시 signup액티비티로 넘어가게끔 연결함.
    
    3. 신체정보입력 화면에서 로그인 화면과 마찬가지로 사용자가 입력한 내용이 무엇인지 확인할 수 있는 토스트메시지 출력. 이후 selectSetting 액티비티로 다시 돌아감.
        3-1. 뒤로가는 화살표 선택 시 selectSetting 액티비티로 돌아갈 수 있게함.
    
    4. edit account inform 액티비티에서 id충돌 문제 확인 -> 16일 저녁 안에 수정 예정
        4-1. 충돌이 일어나지 않은 사용자 이메일 정보만 우선적으로 입력된 내용을 저장하여 보여주는 토스트 메시지 작성
        4-2. 뒤로가는 화살표 선택 시 selectSetting 액티비티로 돌아갈 수 있게함.
    
    5. sign up 액티비티에서 사용자가 입력한 이메일을 확인누를 시 변수 저장 후 토스트로 띄움.(api연결 전 임시장치)
        5-1. 뒤로가기 화살표 선택 시 로그인 화면으로 돌아갈 수 있게 함.
        5-2. 마찬가지로 id충돌문제 확인, 16일 저녁까지 수정완료예정

--- app/src/main/java/com/example/art_run_android/BodyInformEditActivity.kt
+++ app/src/main/java/com/example/art_run_android/BodyInformEditActivity.kt
@@ -1,11 +1,34 @@
 package com.example.art_run_android
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.art_run_android.databinding.ActivityBodyInformEditBinding
 
 class BodyInformEditActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_body_inform_edit)
+        val binding =ActivityBodyInformEditBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btBack.setOnClickListener{
+            val intent = Intent(this,SelectSettingsActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.btConfirm.setOnClickListener{
+            val height=binding.textHeight.text.toString()
+            val weight=binding.textWeight.text.toString()
+            val age = binding.textAge.text.toString()
+
+            val message = "키 : "+height+"cm\n몸무게 : "+weight+"kg\n나이 : "+age
+
+            val toast = Toast.makeText(this,message,Toast.LENGTH_LONG)
+            toast.show()
+            val intent = Intent(this,SelectSettingsActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
--- app/src/main/java/com/example/art_run_android/LoginActivity.kt
+++ app/src/main/java/com/example/art_run_android/LoginActivity.kt
@@ -1,11 +1,31 @@
 package com.example.art_run_android
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.art_run_android.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        val binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btLogin.setOnClickListener{
+            val userID=binding.textUserID.text.toString()
+            val userPassword=binding.textUserPassword.text.toString()
+            val userInfo="ID :"+userID+"\nPassword :"+userPassword
+
+            val toast = Toast.makeText(this,userInfo,Toast.LENGTH_LONG)
+            toast.show()
+            val intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.btSignup.setOnClickListener{
+            val intent = Intent(this,SignUpActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
