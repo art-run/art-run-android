3e28d421c0b7420554e5c436cf053156f3366420
LeeHaiLim <hailim1997@gmail.com>
2022-02-16 13:38:32 +0900

    Api 연결, 그리기

--- app/src/main/java/com/example/art_run_android/FreeRunActivity.kt
+++ app/src/main/java/com/example/art_run_android/FreeRunActivity.kt
@@ -1,18 +1,110 @@
 package com.example.art_run_android
 
+import SetView
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationManager
 import android.os.Bundle
 import android.widget.ImageButton
+import android.widget.LinearLayout
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.model.LatLng
+import kotlinx.android.synthetic.main.activity_freerun.*
+
 
 class FreeRunActivity : BaseActivity() {
+
+    private val PERMISSIONS = arrayOf(
+        Manifest.permission.ACCESS_COARSE_LOCATION,
+        Manifest.permission.ACCESS_FINE_LOCATION)
+
+    private val REQUEST_PERMISSION_CODE = 1
+
+    private val DEFAULT_ZOOM_LEVEL = 17f
+
+    private val CITY_HALL = LatLng(37.5662952, 126.97794509999994)
+
+    private var googleMap: GoogleMap? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_freerun)
+
+        val canvasView = SetView(this)
+        var canvasLayout : LinearLayout = findViewById(R.id.canvasLayout)
+        canvasLayout.addView(canvasView)
+
 
         val startButton: ImageButton = findViewById(R.id.startButton2)
         startButton.setOnClickListener{
             val intent = Intent(this, RunningActivity::class.java)
             startActivity(intent)
         }
+
+        mapView2.onCreate(savedInstanceState)
+
+        if (checkPermissions()) {
+            initMap()
+        } else {
+            ActivityCompat.requestPermissions(this, PERMISSIONS, REQUEST_PERMISSION_CODE)
+        }
+    }
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        initMap()
+    }
+
+    private fun checkPermissions(): Boolean {
+
+        for (permission in PERMISSIONS) {
+            if (ActivityCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
+                return false
+            }
+        }
+        return true
+    }
+
+    @SuppressLint("MissingPermission")
+    fun initMap() {
+        mapView2.getMapAsync {
+
+            googleMap = it
+            it.uiSettings.isMyLocationButtonEnabled = false
+
+            when {
+                checkPermissions() -> {
+                    it.isMyLocationEnabled = true
+                    it.moveCamera(CameraUpdateFactory.newLatLngZoom(getMyLocation(), DEFAULT_ZOOM_LEVEL))
+                }
+                else -> {
+                    it.moveCamera(CameraUpdateFactory.newLatLngZoom(CITY_HALL, DEFAULT_ZOOM_LEVEL))
+                }
+            }
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    fun getMyLocation(): LatLng {
+
+        val locationProvider: String = LocationManager.GPS_PROVIDER
+
+        val locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+
+        val lastKnownLocation: Location? = locationManager.getLastKnownLocation(locationProvider)
+
+        return if (lastKnownLocation != null) {
+            LatLng(lastKnownLocation.latitude, lastKnownLocation.longitude)
+        } else {
+            CITY_HALL
+        }
     }
 }
+
+
